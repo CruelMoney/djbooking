f7d6f179ff4727fc0d873ccd8a01fdcee69ff469
cruelmoney <chrdengso@gmail.com>
2016-12-03 15:11:36 +0100

    Scroll animation

--- src/components/pages/HowItWorks.js
+++ src/components/pages/HowItWorks.js
@@ -8,6 +8,14 @@ import PConfig3 from '../../assets/particlesjs-config-3.json';
 import PConfig4 from '../../assets/particlesjs-config-4.json';
 import PConfig5 from '../../assets/particlesjs-config-5.json';
 
+/*animation stuff*/
+import ScrollAnim from 'rc-scroll-anim';
+import QueueAnim from 'rc-queue-anim';
+import TweenOne from 'rc-tween-one';
+
+const ScrollOverPack = ScrollAnim.OverPack;
+
+
 export default React.createClass({
   themeColor:"#31DAFF",
 
@@ -27,24 +35,25 @@ export default React.createClass({
           <span/>
           <span/>
         </div>
-
         <div className="container">
+        <TweenOne className="tween-one" key="0" style={{opacity: 0}} animation={{ opacity: 1 }}>
           <div style={{position: "absolute"}}>
             <Particles params={PConfig} width="300px" height="300px"/>
-
           </div>
+        </TweenOne>
+
           <div className="col-md-7 col-md-push-4" style={{top:"50px"}}>
-            <h1>How it works</h1>
-            <p>Stripe is the best software platform for running an internet
+            <QueueAnim key="1">
+            <h1 key="title">How it works</h1>
+            <p key="paragraph">Stripe is the best software platform for running an internet
             business. We handle billions of dollars every ythinking businesses around the world. thinking businesses around the world.thinking businesses around the world.ear for forward-
             thinking businesses around the world.
             </p>
+          </QueueAnim>
           </div>
+
         </div>
       </header>
-
-
-
       <div style={{position:"relative"}} >
         <div className="particles-bg" />
         <div className="container">
@@ -55,43 +64,63 @@ export default React.createClass({
             <Particles params={PConfig4} width="390px" height="390px"/>
             <Particles params={PConfig5} width="430px" height="430px"/>
           </div>
-          <div className="how-it-works">
+          <div className="how-it-works" >
             <section>
-              <div>
-                <div className="circle">1</div>
-                <h2>Organizer creates event</h2>
-              </div>
-              <p>Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
-            </section>
+          <div>
+            <div>
+            <div key="a" className="how-to-title">
+              <div className="circle">1</div>
+              <h2>Organizer creates event</h2>
+            </div>
+            <p key="b">Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
+            </div>
+          </div>
+          </section>
             <section>
-              <div>
+              <ScrollOverPack id="page1" className="page1" hideProps={{ 0: { reverse: true } }}>
+              <QueueAnim key="2">
+              <div key="a" className="how-to-title">
                 <div className="circle">2</div>
                 <h2>Qualified DJs return offer</h2>
               </div>
-              <p>Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
-            </section>
+              <p key="b">Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
+              </QueueAnim>
+            </ScrollOverPack>
+          </section>
             <section>
-              <div>
+              <ScrollOverPack id="page1" className="page1" hideProps={{ 0: { reverse: true } }}>
+              <QueueAnim key="3">
+              <div key="a" className="how-to-title">
                 <div className="circle">3</div>
                 <h2>Organizer confirms an offer</h2>
               </div>
-              <p>Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
-            </section>
+              <p key="b">Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
+              </QueueAnim>
+            </ScrollOverPack>
+          </section>
             <section>
-              <div>
+              <ScrollOverPack id="page1" className="page1" hideProps={{ 0: { reverse: true } }}>
+              <QueueAnim key="4">
+              <div key="a" className="how-to-title">
                 <div className="circle">4</div>
                 <h2>DJ plays at event</h2>
               </div>
-              <p>Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
-            </section>
+              <p key="b">Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
+              </QueueAnim>
+            </ScrollOverPack>
+          </section>
             <section>
-              <div>
+              <ScrollOverPack id="page1" className="page1" hideProps={{ 0: { reverse: true } }}>
+              <QueueAnim key="5">
+              <div key="a" className="how-to-title">
                 <div className="circle">5</div>
                 <h2>Payout is released</h2>
               </div>
-              <p>Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
-            </section>
-            </div>
+              <p key="b">Stripe builds the most powerful and flexible tools for internet commerce. Whether you’re creating a subscription service, an on-demand marketplace, an e-commerce store, or a crowdfunding platform, Stripe’s meticulously-designed APIs and unmatched functionality help you create the best possible product for your users. Hundreds of thousands of the world’s </p>
+              </QueueAnim>
+            </ScrollOverPack>
+          </section>
+          </div>
         </div>
       </div>
       <div className="prefooter">
--- src/components/pages/Signup.js
+++ src/components/pages/Signup.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import SignUpForm from '../../containers/SignupForm'
-
-
-
-var containerStyle = {
-  maxWidth: '700px',
-  paddingLeft: '40px',
-  paddingRight: '40px',
-  margin: 'auto',
-  marginBottom: '50px',
-  marginTop: '150px'
-}
+import { Link } from 'react-router';
 
 export default React.createClass({
   render() {
-    return  <div style={containerStyle}>
+    return  <div >
+      <header className=""
+
+      >
+      <div id="stripes">
+        <span/>
+        <span/>
+        <span className="white"/>
+        <span/>
+      </div>
+      <div className="container">
+
+      </div>
+
+    </header>
       <SignUpForm/>
+        <div className="prefooter">
+          <div className="container">
+            <div className="row">
+            <div className="action-title col-md-7">
+              <span style={{color:this.themeColor}}>Ready to get started?</span>
+              Arrange an event, or become a DJ.
+            </div>
+            <div className="col-md-5 action-buttons">
+              <Link style={{background:this.themeColor, color:"#FFFFFF"}} className="button" to="/">Arrange event</Link>
+              <Link style={{background:"#FFFFFF", color:this.themeColor}} className="button" to="/signup">Become DJ</Link>
+            </div>
+            </div>
+          </div>
+        </div>
     </div>
   }
 })
