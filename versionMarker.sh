5c346374f6f7f4d0f5478243a738da3ec694c970
Christopher Dengsø <chrdengso@gmail.com>
2017-10-28 14:29:16 +0800

    updated stuff

--- versionMarker.sh
+++
@@ -1,5 +0,0 @@
-branch=$(git symbolic-ref HEAD | sed -e 's,.*/\(.*\),\1,')
-commit=$(git rev-parse HEAD)
-ghead -n -2 .env > temp ; mv temp .env
-echo REACT_APP_BRANCH=$branch >> .env
-echo REACT_APP_COMMIT=$commit >> .env
