0510e26a277d09fb2b6d19ae552c8ab6584929e0
Christopher Dengsø <chrdengso@gmail.com>
2017-10-30 14:24:25 +0800

    Error page

--- src/components/common/ErrorPage.js
+++ src/components/common/ErrorPage.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import EmptyPage from './EmptyPage'
-import Footer from './Footer';
 
 export default class ErrorPage extends Component{
   constructor(props) {
@@ -11,30 +10,19 @@ export default class ErrorPage extends Component{
   componentDidCatch(error, info) {
     // Display fallback UI
     this.setState({ hasError: true });
-    console.log(error, info);
   }
 
   render(){
     if (this.state.hasError) {
-    return <div className="not-found-screen">
+    return <div className="error-screen">
         <EmptyPage 
           title={`Something went wrong`}
           message={
-            <div>Sorry, something went wrong.<br/>
+            <span>Sorry, something went wrong.<br/>
               Try reloading the page or go back.<br/>
               <a style={{marginTop: '20px'}} className="button" href="/">GO BACK</a> 
-            </div>
+            </span>
           }
-        />
-        <Footer
-          color={"#31DAFF"}
-          noSkew={true}
-          firstTo="/"
-          secondTo="/signup"
-          firstLabel="Arrange event"
-          secondLabel="Become DJ"
-          title="Ready to get started?"
-          subTitle="Arrange an event, or apply to become a DJ."
         />
     </div>
     }else{
