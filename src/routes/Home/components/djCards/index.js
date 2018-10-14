dce544b44a0a79352f5cc864ef7cb05ab872fdd0
Christopher Dengsø <chrdengso@gmail.com>
2018-10-15 01:37:35 +0700

    fix front page animation

--- src/routes/Home/components/djCards/index.js
+++ src/routes/Home/components/djCards/index.js
@@ -1,80 +1,72 @@
-import React, {Component} from 'react'
-import './index.css'
-import {DJs} from '../../../../constants/constants';
-import shuffle from 'lodash.shuffle'
-import DJCard from '../../../../components/common/DJCard';
-export default class DJCards extends Component{
-    
-    animation = null;
-    cardsLoaded = false;
-    cards = [];
+import React, { Component } from "react";
+import "./index.css";
+import { DJs } from "../../../../constants/constants";
+import shuffle from "lodash.shuffle";
+import DJCard from "../../../../components/common/DJCard";
+export default class DJCards extends Component {
+	animation = null;
+	cardsLoaded = false;
+	cards = [];
 
-    state={
-        currentCards: [],
-        restCards: [],
-        cards: []
-    }
+	state = {
+		currentCards: [],
+		restCards: [],
+		cards: []
+	};
 
-    componentWillUnmount(){
-        this.animation && clearInterval(this.animation);
-    }
+	componentWillUnmount() {
+		this.animation && clearInterval(this.animation);
+	}
 
-    componentWillMount(){
-        this.cards = shuffle(DJs.map((dj, idx) => <DJCard 
-            { ...this.props }
-            onLoad={()=>this.cardsLoaded++} 
-            key={`dj-card-${idx}`} 
-            dj={dj} 
-            />))
-    }
+	componentWillMount() {
+		this.cards = shuffle(
+			DJs.map((dj, idx) => (
+				<DJCard
+					{...this.props}
+					onLoad={() => this.cardsLoaded++}
+					key={`dj-card-${idx}`}
+					dj={dj}
+				/>
+			))
+		);
+	}
 
-    componentDidMount(){
-        const currentCards = this.cards.slice(0,11);
-        const restCards = this.cards.slice(11,   this.cards.length-1);
+	componentDidMount() {
+		const currentCards = this.cards.slice(0, 11);
+		const restCards = this.cards.slice(11, this.cards.length - 1);
 
-        this.setState({
-            currentCards,
-            restCards
-        });
+		this.setState({
+			currentCards,
+			restCards
+		});
 
-        this.animation = setInterval(this.showNewCard, 4500);
-    }
-    
+		// this.animation = setInterval(this.showNewCard, 4500);
+	}
 
-    showNewCard = () => {
+	showNewCard = () => {
+		const { currentCards, restCards } = this.state;
+		if (this.cardsLoaded >= currentCards.length) {
+			const idx = Math.floor(Math.random() * currentCards.length);
+			const newCard = restCards.pop();
+			const replacedCard = currentCards[idx];
+			currentCards[idx] = newCard;
+			this.setState({
+				restCards: [replacedCard, ...restCards],
+				currentCards
+			});
+		}
+	};
 
-        const { currentCards, restCards } = this.state;
-        if(this.cardsLoaded >= currentCards.length){
-            const idx = Math.floor(Math.random()*currentCards.length);
-            const newCard = restCards.pop();
-            const replacedCard = currentCards[idx];
-            currentCards[idx] = newCard;
-            this.setState({
-                restCards: [replacedCard, ...restCards],
-                currentCards
-            });
-        }      
-    }
+	render() {
+		const { currentCards } = this.state;
 
-    render() {
-    const { currentCards } = this.state;
-
-    return (
-        <div className="dj-cards">
-            <div>
-                {currentCards.slice(0,3)}
-            </div>
-            <div>
-                {currentCards.slice(3,6)}
-            </div>
-            <div>
-                {currentCards.slice(6,9)}
-            </div>
-            <div>
-                {currentCards.slice(9,11)}
-            </div>
-            
-        </div>
-    )
-  }
+		return (
+			<div className="dj-cards">
+				<div>{currentCards.slice(0, 3)}</div>
+				<div>{currentCards.slice(3, 6)}</div>
+				<div>{currentCards.slice(6, 9)}</div>
+				<div>{currentCards.slice(9, 11)}</div>
+			</div>
+		);
+	}
 }
