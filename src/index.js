423e3c9962cf813490d341d590d0217ffd1d50f0
Christopher Dengsø <chrdengso@gmail.com>
2019-07-04 14:09:11 +0800

    sentry

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { hydrate, render } from "react-dom";
 import Router from "./BrowserRouter";
 import "./css/style.scss";
+import * as Sentry from "@sentry/browser";
+
+Sentry.init({
+	dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082"
+});
+
 // import registerServiceWorker from "./utils/ServiceWorker";
 
 const rootElement = document.getElementById("root");
