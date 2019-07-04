16e5465d2a52190c9570f95935ebd553ea174bf9
Christopher Dengsø <chrdengso@gmail.com>
2019-07-04 14:12:08 +0800

    sentry

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ import Router from "./BrowserRouter";
 import "./css/style.scss";
 import * as Sentry from "@sentry/browser";
 
-Sentry.init({
-	dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082"
-});
+const production = process.env.NODE_ENV === "production";
+
+if (production) {
+	Sentry.init({
+		dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082",
+		environment: process.env.REACT_APP_CUEUP_DEV_CALLBACK_DOMAIN
+	});
+}
 
 // import registerServiceWorker from "./utils/ServiceWorker";
 
