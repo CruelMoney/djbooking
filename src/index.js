7ea4dde9a1a3706e33659b7b3bbeb1ee80c2a622
Christopher Dengsø <chrdengso@gmail.com>
2019-07-04 14:27:34 +0800

    notify sentry

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import * as Sentry from "@sentry/browser";
 const production = process.env.NODE_ENV === "production";
 
 if (production) {
+	console.log("Initializing Sentry");
 	Sentry.init({
 		dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082",
 		environment: process.env.REACT_APP_CUEUP_DEV_CALLBACK_DOMAIN
