2c954bcca5eea45c81383ac9b46906b07b5ca679
Christopher Dengsø <chrdengso@gmail.com>
2019-08-09 22:19:25 +0800

    set cookie

--- src/utils/AuthService.js
+++ src/utils/AuthService.js
@@ -4,11 +4,13 @@ class AuthService extends EventEmitter {
 	setSession(token) {
 		// Set the time that the access token will expire at
 		localStorage.setItem("token", token);
+		document.cookie = `x-token=${token}; expires = Thu, 01 Jan 2090 00:00:00 GMT`;
 	}
 
 	logout() {
 		// Clear access token and ID token from local storage
 		localStorage.removeItem("token");
+		document.cookie = `x-token=; expires = Thu, 01 Jan 1970 00:00:00 GMT`;
 	}
 
 	loggedIn() {
