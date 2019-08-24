38f749ad769803e108ab035faac48d230464de00
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-08-24 20:16:04 +0700

    init

--- src/index.js
+++ src/index.js
@@ -1,26 +1,21 @@
-import React from "react";
-import { hydrate, render } from "react-dom";
-import Router from "./BrowserRouter";
-import "./css/style.scss";
-import * as Sentry from "@sentry/browser";
 
-const production = process.env.NODE_ENV === "production";
+import React from 'react'
+import { render } from 'react-dom';
+import DJCard from './components/common/DJCard';
 
-if (production) {
-	console.log("Initializing Sentry");
-	Sentry.init({
-		enabled: production,
-		dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082",
-		environment: process.env.REACT_APP_CUEUP_DEV_CALLBACK_DOMAIN
-	});
+const Index = () => {
+    return (
+        <div>
+            <DJCard
+                
+            > 
+                
+            </DJCard>
+        </div>
+    )
 }
 
-// import registerServiceWorker from "./utils/ServiceWorker";
+
 
 const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-	hydrate(<Router />, rootElement);
-} else {
-	render(<Router />, rootElement);
-}
-// registerServiceWorker();
+render(<Index />, rootElement);
---
+++ src/index2.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { hydrate, render } from "react-dom";
+import Router from "./BrowserRouter";
+import "./css/style.scss";
+import * as Sentry from "@sentry/browser";
+
+const production = process.env.NODE_ENV === "production";
+
+if (production) {
+	console.log("Initializing Sentry");
+	Sentry.init({
+		enabled: production,
+		dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082",
+		environment: process.env.REACT_APP_CUEUP_DEV_CALLBACK_DOMAIN
+	});
+}
+
+// import registerServiceWorker from "./utils/ServiceWorker";
+
+const rootElement = document.getElementById("root");
+if (rootElement.hasChildNodes()) {
+	hydrate(<Router />, rootElement);
+} else {
+	render(<Router />, rootElement);
+}
+// registerServiceWorker();
