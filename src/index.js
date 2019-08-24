124e73ae7a83fc87f829397716b2b632dd9aac03
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-08-24 21:48:30 +0700

    normal index

--- src/index.js
+++ src/index.js
@@ -1,80 +1,26 @@
 import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Provider } from "react-redux";
-import getMuiTheme from "material-ui/styles/getMuiTheme";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import { HelmetProvider } from "react-helmet-async";
-import ReactModal from "react-modal";
-import ApolloProvider from "./ApolloProvider";
-import DjCard from "./routes/Event/components/blocks/DJCard";
-import profilePicture from "../src/assets/images/oliver.jpg";
+import { hydrate, render } from "react-dom";
+import Router from "./BrowserRouter";
 import "./css/style.scss";
-import styled from "styled-components";
+import * as Sentry from "@sentry/browser";
 
-const theme = getMuiTheme();
+const production = process.env.NODE_ENV === "production";
 
-ReactModal.setAppElement("#root");
+if (production) {
+	console.log("Initializing Sentry");
+	Sentry.init({
+		enabled: production,
+		dsn: "https://800ac4dbef6c44bcb65af9fddad9f964@sentry.io/1490082",
+		environment: process.env.REACT_APP_CUEUP_DEV_CALLBACK_DOMAIN
+	});
+}
 
-const dj = {
-  artistName: "DJ Spinsø",
-  email: "chrdengso@gmail.com",
-
-  picture: {
-    path: profilePicture
-  },
-  userMetadata: {
-    phone: "+4524658061",
-    firstName: "Christopher",
-    lastName: "Last",
-    bio:
-      "Collect all receipts, and enter information into accounting software (send it to Xero involveret and process from there)  Also go through private bank to check nothing is missed. Set revolut to payment on all relevant sites. Create blog posts Community building"
-  }
-};
-const organizer = {
-  picture: {
-    path: profilePicture
-  },
-  userMetadata: {
-    firstName: "Christopher",
-    lastName: "Tusch"
-  }
-};
-const gig = {
-  dj,
-  status: "ACCEPTED",
-  offer: {
-    totalPayment: {
-      formatted: "IDR 3.000.000"
-    }
-  }
-};
-const event = {
-  organizer
-};
-
-const Wrapper = styled.div`
-  margin: 15px;
-`;
-
-const Index = () => {
-  return (
-    <Wrapper>
-      <DjCard idx={0} theEvent={event} gig={gig} translate={s => s} />
-    </Wrapper>
-  );
-};
+// import registerServiceWorker from "./utils/ServiceWorker";
 
 const rootElement = document.getElementById("root");
-render(
-  <ApolloProvider>
-    <Router>
-      <MuiThemeProvider muiTheme={theme}>
-        <HelmetProvider>
-          <Index />
-        </HelmetProvider>
-      </MuiThemeProvider>
-    </Router>
-  </ApolloProvider>,
-  rootElement
-);
+if (rootElement.hasChildNodes()) {
+	hydrate(<Router />, rootElement);
+} else {
+	render(<Router />, rootElement);
+}
+// registerServiceWorker();
