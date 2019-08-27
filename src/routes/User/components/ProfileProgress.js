4ccd2ff619822a434af048aefe887b7108a825d7
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-08-27 14:05:21 +0800

    complete profile dj only

--- src/routes/User/components/ProfileProgress.js
+++ src/routes/User/components/ProfileProgress.js
@@ -8,137 +8,137 @@ import { NavLink } from "react-router-dom";
 import { SimpleSharing } from "../../../components/common/Sharing-v2";
 
 const checks = [
-	{
-		label: "Add profile picture",
-		check: u => !!u.picture && !u.picture.path.includes("default-profile-pic"),
-		linkTo: "settings#profile"
-	},
-	{
-		label: "Add artist name",
-		check: u => !!u.artistName,
-		linkTo: "settings#profile"
-	},
-	{
-		label: "Highlight a testimonial",
-		check: u => !!u.highlightedReview,
-		linkTo: "reviews"
-	},
-	{
-		label: "Add payout information",
-		check: u => !!u.userMetadata.bankAccount,
-		linkTo: "settings#preferences"
-	},
-	{
-		label: "Download the app",
-		check: u => !!u.appMetadata.hasInstalledApp,
-		linkTo: "/signup"
-	},
-	{
-		label: "Add photos or connect Instagram",
-		check: u => u.media.edges.length > 0,
-		linkTo: "photos"
-	}
-	// {
-	// 	label: "Verify identity",
-	// 	check: u => !!u.appMetadata.identityVerified,
-	// 	linkTo: "settings#preferences"
-	// }
+  {
+    label: "Add profile picture",
+    check: u => !!u.picture && !u.picture.path.includes("default-profile-pic"),
+    linkTo: "settings#profile"
+  },
+  {
+    label: "Add artist name",
+    check: u => !!u.artistName,
+    linkTo: "settings#profile"
+  },
+  {
+    label: "Highlight a testimonial",
+    check: u => !!u.highlightedReview,
+    linkTo: "reviews"
+  },
+  {
+    label: "Add payout information",
+    check: u => !!u.userMetadata.bankAccount,
+    linkTo: "settings#preferences"
+  },
+  {
+    label: "Download the app",
+    check: u => !!u.appMetadata.hasInstalledApp,
+    linkTo: "/signup"
+  },
+  {
+    label: "Add photos or connect Instagram",
+    check: u => u.media.edges.length > 0,
+    linkTo: "photos"
+  }
+  // {
+  // 	label: "Verify identity",
+  // 	check: u => !!u.appMetadata.identityVerified,
+  // 	linkTo: "settings#preferences"
+  // }
 ];
 
 const ProgressItemText = styled(SmallHeader)`
-	font-size: 15px;
-	color: ${({ done }) => (done ? "#98A4B3" : "#4D6480")};
-	text-decoration: ${({ done }) => (done ? "line-through" : "none")};
-	line-height: 15px;
-	margin-bottom: 0;
-	margin-left: 6px;
+  font-size: 15px;
+  color: ${({ done }) => (done ? "#98A4B3" : "#4D6480")};
+  text-decoration: ${({ done }) => (done ? "line-through" : "none")};
+  line-height: 15px;
+  margin-bottom: 0;
+  margin-left: 6px;
 `;
 
 const ProgressItem = ({ label, done, linkTo }) => {
-	const Content = (
-		<Row style={{ marginBottom: "15px" }} middle>
-			{done ? (
-				<CheckCircleDone color={"#50E3C2"} fontSize={"20px"} />
-			) : (
-				<CheckCircle color={"#4D6480"} fontSize={"20px"} />
-			)}
-			<ProgressItemText done={done}>{label}</ProgressItemText>
-		</Row>
-	);
+  const Content = (
+    <Row style={{ marginBottom: "15px" }} middle>
+      {done ? (
+        <CheckCircleDone color={"#50E3C2"} fontSize={"20px"} />
+      ) : (
+        <CheckCircle color={"#4D6480"} fontSize={"20px"} />
+      )}
+      <ProgressItemText done={done}>{label}</ProgressItemText>
+    </Row>
+  );
 
-	if (done) {
-		return Content;
-	}
+  if (done) {
+    return Content;
+  }
 
-	return <NavLink to={linkTo}>{Content}</NavLink>;
+  return <NavLink to={linkTo}>{Content}</NavLink>;
 };
 
 const ProfileProgress = ({ user }) => {
-	const items = checks
-		.map(c => ({ ...c, done: c.check(user) }))
-		.sort((a, b) => b.done - a.done);
+  const items = checks
+    .map(c => ({ ...c, done: c.check(user) }))
+    .sort((a, b) => b.done - a.done);
 
-	const progress = items.filter(c => c.done).length / items.length;
+  const progress = items.filter(c => c.done).length / items.length;
 
-	if (progress === 1) {
-		return (
-			<SimpleSharing shareUrl={user && `/user/${user.permalink}/overview}]`} />
-		);
-	}
-	return (
-		<Col
-			style={{
-				alignItems: "flex-start",
-				marginBottom: "30px"
-			}}
-		>
-			<Body>Complete your profile</Body>
-			<ProgressBar progress={progress} />
+  if (progress === 1 || !user.isDj) {
+    return (
+      <SimpleSharing shareUrl={user && `/user/${user.permalink}/overview}]`} />
+    );
+  }
+  return (
+    <Col
+      style={{
+        alignItems: "flex-start",
+        marginBottom: "30px"
+      }}
+    >
+      <Body>Complete your profile</Body>
+      <ProgressBar progress={progress} />
 
-			{items.map(c => (
-				<ProgressItem key={c.label} {...c} />
-			))}
-		</Col>
-	);
+      {items.map(c => (
+        <ProgressItem key={c.label} {...c} />
+      ))}
+    </Col>
+  );
 };
 
 const BarTrack = styled.div`
-	background-color: #d3f8f0;
-	height: 12px;
-	width: 100%;
-	border-radius: 6px;
+  background-color: #d3f8f0;
+  height: 12px;
+  width: 100%;
+  border-radius: 6px;
 `;
 
 const BarFill = styled.div`
-	height: 12px;
-	background: #50e3c2;
-	box-shadow: 0 0 1px 1px rgba(255, 255, 255, 0.5), 0 0 8px 0 #00ffc6;
-	border-radius: 6px;
-	transition: all 500ms cubic-bezier(0.785, 0.135, 0.15, 0.86);
+  height: 12px;
+  background: #50e3c2;
+  box-shadow: 0 0 1px 1px rgba(255, 255, 255, 0.5), 0 0 8px 0 #00ffc6;
+  border-radius: 6px;
+  transition: all 500ms cubic-bezier(0.785, 0.135, 0.15, 0.86);
 `;
 
 const ProgressBar = ({ progress }) => {
-	const [renderProgress, setRenderProgress] = useState(0);
+  const [renderProgress, setRenderProgress] = useState(0);
 
-	useEffect(() => {
-		setRenderProgress(progress);
-	}, [progress]);
+  useEffect(() => {
+    setRenderProgress(progress);
+  }, [progress]);
 
-	return (
-		<Row middle style={{ marginBottom: "15px", width: "100%" }}>
-			<BarTrack>
-				<BarFill style={{ width: renderProgress * 100 + "%" }} />
-			</BarTrack>
-			<SmallBold
-				style={{
-					marginBottom: 0,
-					marginLeft: "6px"
-				}}
-			>
-				{Math.round(progress * 100)}%
-			</SmallBold>
-		</Row>
-	);
+  return (
+    <Row middle style={{ marginBottom: "15px", width: "100%" }}>
+      <BarTrack>
+        <BarFill style={{ width: renderProgress * 100 + "%" }} />
+      </BarTrack>
+      <SmallBold
+        style={{
+          marginBottom: 0,
+          marginLeft: "6px"
+        }}
+      >
+        {Math.round(progress * 100)}%
+      </SmallBold>
+    </Row>
+  );
 };
 
 export default ProfileProgress;
