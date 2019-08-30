f8b3be069ccf7ab39af6b6fffe61152125e58391
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-08-30 19:59:10 +0800

    withMap

--- src/components/common/Map.js
+++ src/components/common/Map.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { withGoogleMap, GoogleMap, Circle } from "react-google-maps";
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Circle
+} from "react-google-maps";
 import connectToForm from "../higher-order/connectToForm";
 
 /*
@@ -13,187 +18,187 @@ import connectToForm from "../higher-order/connectToForm";
  * https://facebook.github.io/react/blog/2015/09/10/react-v0.14-rc1.html#stateless-function-components
  */
 class SimpleMap extends Component {
-	circle = null;
+  circle = null;
 
-	marker = {
-		position: { lat: 56.0, lng: 10.0 },
-		radius: 250000,
-		key: `Denmark`,
-		defaultAnimation: 2
-	};
+  marker = {
+    position: { lat: 56.0, lng: 10.0 },
+    radius: 250000,
+    key: `Denmark`,
+    defaultAnimation: 2
+  };
 
-	constructor(props) {
-		super(props);
-		const short = !!this.props.value.lat;
+  constructor(props) {
+    super(props);
+    const short = !!this.props.value.lat;
 
-		const position = short
-			? {
-					...this.props.value
-			  }
-			: {
-					lat: this.props.value.latitude,
-					lng: this.props.value.longitude
-			  };
+    const position = short
+      ? {
+          ...this.props.value
+        }
+      : {
+          lat: this.props.value.latitude,
+          lng: this.props.value.longitude
+        };
 
-		this.marker = {
-			position,
-			radius: props.radius,
-			key: Date.now(),
-			defaultAnimation: 2
-		};
-	}
+    this.marker = {
+      position,
+      radius: props.radius,
+      key: Date.now(),
+      defaultAnimation: 2
+    };
+  }
 
-	shouldComponentUpdate(nextProps, nextState) {
-		if (nextProps.editable !== this.props.editable) {
-			return true;
-		}
-		return false;
-	}
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextProps.editable !== this.props.editable) {
+      return true;
+    }
+    return false;
+  }
 
-	handleRadiusChange = circle => {
-		const radius = circle.getRadius();
-		if (this.context.updateValue) {
-			this.context.updateValue(this.props.radiusName, radius);
-		}
-		if (this.props.onRadiusChange) {
-			this.props.onRadiusChange(radius);
-		}
-	};
+  handleRadiusChange = circle => {
+    const radius = circle.getRadius();
+    if (this.context.updateValue) {
+      this.context.updateValue(this.props.radiusName, radius);
+    }
+    if (this.props.onRadiusChange) {
+      this.props.onRadiusChange(radius);
+    }
+  };
 
-	handleLocationChange = circle => {
-		const data = {
-			lat: circle.getCenter().lat(),
-			lng: circle.getCenter().lng()
-		};
+  handleLocationChange = circle => {
+    const data = {
+      lat: circle.getCenter().lat(),
+      lng: circle.getCenter().lng()
+    };
 
-		if (this.context.updateValue) {
-			this.context.updateValue(this.props.locationName, data);
-		}
-		if (this.props.onCoordinatesChange) {
-			this.props.onCoordinatesChange(data);
-		}
-	};
+    if (this.context.updateValue) {
+      this.context.updateValue(this.props.locationName, data);
+    }
+    if (this.props.onCoordinatesChange) {
+      this.props.onCoordinatesChange(data);
+    }
+  };
 
-	getZoomLevel(radius) {
-		if (radius === 0) return 10;
-		var scale = radius / 500;
-		var zoomLevel = 15 - Math.log(scale) / Math.log(2);
-		return parseInt(zoomLevel, 10);
-	}
+  getZoomLevel(radius) {
+    if (radius === 0) return 10;
+    var scale = radius / 500;
+    var zoomLevel = 15 - Math.log(scale) / Math.log(2);
+    return parseInt(zoomLevel, 10);
+  }
 
-	render() {
-		const { mapOptions } = this.props;
-		return (
-			<GoogleMap
-				key={this.props.key}
-				defaultZoom={this.getZoomLevel(this.props.radius)}
-				defaultCenter={this.props.defaultCenter || this.marker.position}
-				streetViewControl={false}
-				defaultOptions={{
-					scrollwheel: false,
-					streetViewControl: false,
-					mapTypeControl: false,
+  render() {
+    const { mapOptions } = this.props;
+    return (
+      <GoogleMap
+        key={this.props.key}
+        defaultZoom={this.getZoomLevel(this.props.radius)}
+        defaultCenter={this.props.defaultCenter || this.marker.position}
+        streetViewControl={false}
+        defaultOptions={{
+          scrollwheel: false,
+          streetViewControl: false,
+          mapTypeControl: false,
 
-					styles: [
-						{
-							stylers: [{ visibility: "off" }]
-						},
-						{
-							featureType: "water",
-							stylers: [{ visibility: "simplified" }, { color: "#ffffff" }]
-						},
-						{
-							featureType: "landscape",
-							stylers: [{ visibility: "on" }, { color: "#32325D" }]
-						},
-						{
-							featureType: "administrative.locality",
-							elementType: "labels",
-							stylers: [{ color: "#ffffff" }, { visibility: "on" }]
-						},
-						{
-							featureType: "administrative.locality",
-							elementType: "labels.text.stroke",
-							stylers: [{ color: "#32325D" }, { visibility: "on" }]
-						},
-						{
-							featureType: "road.arterial",
-							elementType: "geometry",
-							stylers: [
-								{ visibility: this.props.hideRoads ? "off" : "simplified" }
-							]
-						},
-						{
-							featureType: "administrative.province",
-							stylers: [{ visibility: "on" }]
-						},
-						{
-							featureType: "administrative.province",
-							elementType: "labels.text.fill",
-							stylers: [{ color: "#ffffff" }, { visibility: "on" }]
-						},
-						{
-							featureType: "administrative.province",
-							elementType: "labels.text.stroke",
-							stylers: [{ color: "#32325D" }, { visibility: "on" }]
-						},
-						{
-							featureType: "administrative.country",
-							stylers: [{ visibility: "on" }]
-						},
-						{
-							featureType: "administrative.country",
-							elementType: "labels.text.fill",
-							stylers: [{ color: "#ffffff" }, { visibility: "on" }]
-						},
-						{
-							featureType: "administrative.country",
-							elementType: "labels.text.stroke",
-							stylers: [{ color: "#32325D" }, { visibility: "on" }]
-						}
-					],
-					...mapOptions
-				}}
-			>
-				{!!this.props.noCircle ? null : (
-					<Circle
-						ref={c => (this.circle = c)}
-						defaultOptions={{
-							fillColor: this.props.color || this.context.color,
-							strokeWeight: 0,
-							suppressUndo: true
-						}}
-						editable={this.props.editable}
-						center={
-							this.circle ? this.circle.getCenter() : this.marker.position
-						}
-						radius={this.circle ? this.circle.getRadius() : this.marker.radius}
-						onCenterChanged={() => this.handleLocationChange(this.circle)}
-						onRadiusChanged={() => this.handleRadiusChange(this.circle)}
-					/>
-				)}
-			</GoogleMap>
-		);
-	}
+          styles: [
+            {
+              stylers: [{ visibility: "off" }]
+            },
+            {
+              featureType: "water",
+              stylers: [{ visibility: "simplified" }, { color: "#ffffff" }]
+            },
+            {
+              featureType: "landscape",
+              stylers: [{ visibility: "on" }, { color: "#32325D" }]
+            },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels",
+              stylers: [{ color: "#ffffff" }, { visibility: "on" }]
+            },
+            {
+              featureType: "administrative.locality",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#32325D" }, { visibility: "on" }]
+            },
+            {
+              featureType: "road.arterial",
+              elementType: "geometry",
+              stylers: [
+                { visibility: this.props.hideRoads ? "off" : "simplified" }
+              ]
+            },
+            {
+              featureType: "administrative.province",
+              stylers: [{ visibility: "on" }]
+            },
+            {
+              featureType: "administrative.province",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#ffffff" }, { visibility: "on" }]
+            },
+            {
+              featureType: "administrative.province",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#32325D" }, { visibility: "on" }]
+            },
+            {
+              featureType: "administrative.country",
+              stylers: [{ visibility: "on" }]
+            },
+            {
+              featureType: "administrative.country",
+              elementType: "labels.text.fill",
+              stylers: [{ color: "#ffffff" }, { visibility: "on" }]
+            },
+            {
+              featureType: "administrative.country",
+              elementType: "labels.text.stroke",
+              stylers: [{ color: "#32325D" }, { visibility: "on" }]
+            }
+          ],
+          ...mapOptions
+        }}
+      >
+        {!!this.props.noCircle ? null : (
+          <Circle
+            ref={c => (this.circle = c)}
+            defaultOptions={{
+              fillColor: this.props.color || this.context.color,
+              strokeWeight: 0,
+              suppressUndo: true
+            }}
+            editable={this.props.editable}
+            center={
+              this.circle ? this.circle.getCenter() : this.marker.position
+            }
+            radius={this.circle ? this.circle.getRadius() : this.marker.radius}
+            onCenterChanged={() => this.handleLocationChange(this.circle)}
+            onRadiusChanged={() => this.handleRadiusChange(this.circle)}
+          />
+        )}
+      </GoogleMap>
+    );
+  }
 }
 
 SimpleMap.contextTypes = {
-	color: PropTypes.string,
-	updateValue: PropTypes.func,
-	registerReset: PropTypes.func
+  color: PropTypes.string,
+  updateValue: PropTypes.func,
+  registerReset: PropTypes.func
 };
-
-const SmartMap = connectToForm(withGoogleMap(SimpleMap));
+const SmartMap = connectToForm(withScriptjs(withGoogleMap(SimpleMap)));
 
 const WrappedMap = props => {
-	return (
-		<SmartMap
-			{...props}
-			containerElement={<div style={{ height: props.height || `500px` }} />}
-			mapElement={<div style={{ height: `100%` }} />}
-			loadingElement={<div style={{ height: `100%` }} />}
-		/>
-	);
+  return (
+    <SmartMap
+      {...props}
+      googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQNiY4yM2E0h4SfSTw3khcr9KYS0BgVgQ&libraries=geometry,places,visualization,geocode"
+      containerElement={<div style={{ height: props.height || `500px` }} />}
+      mapElement={<div style={{ height: `100%` }} />}
+      loadingElement={<div style={{ height: `100%` }} />}
+    />
+  );
 };
 
 export default WrappedMap;
