ca5f6f209b54e4d76bd29f6e7573f7f42198e1dc
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-08-30 20:30:00 +0800

    fixed tooltip problem

--- src/components/Tooltip.js
+++ src/components/Tooltip.js
@@ -4,42 +4,43 @@ import styled from "styled-components";
 import ReactDOM from "react-dom";
 
 const ToolTipBox = styled.div`
-	opacity: ${({ showing }) => (showing ? 1 : 0)};
-	pointer-events: none;
-	background-color: #fff;
-	border-radius: 4px;
-	padding: 1em;
-	color: #98a4b3;
-	max-width: 300px;
-	box-shadow: 0 1px 4px 0 rgba(152, 164, 179, 0.6);
+  opacity: ${({ showing }) => (showing ? 1 : 0)};
+  pointer-events: none;
+  background-color: #fff;
+  border-radius: 4px;
+  padding: 1em;
+  color: #98a4b3;
+  max-width: 300px;
+  box-shadow: 0 1px 4px 0 rgba(152, 164, 179, 0.6);
 `;
 
 const Tooltip = ({ children, text }) => {
-	const [showing, setShowing] = useState(false);
-	const close = () => setShowing(false);
-	const open = () => setShowing(true);
+  const [showing, setShowing] = useState(false);
+  const close = () => setShowing(false);
+  const open = () => setShowing(true);
 
-	return (
-		<Manager>
-			<Reference>{({ ref }) => children({ ref, close, open })}</Reference>
-			{typeof document !== "undefined" &&
-				ReactDOM.createPortal(
-					<Popper placement="auto">
-						{({ ref, style, placement }) => (
-							<ToolTipBox
-								ref={ref}
-								style={style}
-								data-placement={placement}
-								showing={showing}
-							>
-								<p style={{ marginBottom: 0 }}>{text}</p>
-							</ToolTipBox>
-						)}
-					</Popper>,
-					document.querySelector("#tooltip-portal")
-				)}
-		</Manager>
-	);
+  return (
+    <Manager>
+      <Reference>{({ ref }) => children({ ref, close, open })}</Reference>
+      {typeof document !== "undefined" &&
+        showing &&
+        ReactDOM.createPortal(
+          <Popper placement="auto">
+            {({ ref, style, placement }) => (
+              <ToolTipBox
+                ref={ref}
+                style={style}
+                data-placement={placement}
+                showing={showing}
+              >
+                <p style={{ marginBottom: 0 }}>{text}</p>
+              </ToolTipBox>
+            )}
+          </Popper>,
+          document.querySelector("#tooltip-portal")
+        )}
+    </Manager>
+  );
 };
 
 export default Tooltip;
