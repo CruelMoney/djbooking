ca04fb51f435548cee931d929f4ce9d1ceefa23e
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-09-10 18:00:20 +0800

    avatar

--- src/components/common/UserMenuItem.js
+++ src/components/common/UserMenuItem.js
@@ -1,64 +1,60 @@
 import React, { Component } from "react";
 import { notificationService } from "../../utils/NotificationService";
 import { connect } from "react-redux";
+import { Avatar } from "../Blocks";
 
 class UserMenuItem extends Component {
-	constructor(props) {
-		super(props);
-		notificationService.init(props.userId);
-	}
+  constructor(props) {
+    super(props);
+    notificationService.init(props.userId);
+  }
 
-	render() {
-		//Cut length if too long
-		function getLabel(name) {
-			if (name.length > 20) {
-				return name.substring(0, 20) + "...";
-			} else {
-				return name;
-			}
-		}
+  render() {
+    //Cut length if too long
+    function getLabel(name) {
+      if (name.length > 20) {
+        return name.substring(0, 20) + "...";
+      } else {
+        return name;
+      }
+    }
 
-		const styles = {
-			image: {
-				backgroundImage: "url(" + this.props.picture + ")",
-				backgroundRepeat: "no-repeat",
-				backgroundPosition: "center",
-				backgroundSize: "auto 100%",
-				width: "30px",
-				height: "30px",
-				marginRight: "4px",
-				borderRadius: "50%",
-				display: "inline-block",
-				position: "relative"
-			},
-			flex: {
-				display: "flex",
-				alignItems: "center"
-			}
-		};
+    const styles = {
+      image: {
+        width: "30px",
+        height: "30px",
 
-		return (
-			<div className="menu-user" style={styles.flex}>
-				<div style={styles.image}>
-					{this.props.notifications > 0 ? (
-						<div className={"notification-bubble"}>
-							{this.props.notifications}
-						</div>
-					) : null}
-				</div>
-				<div style={{ display: "inline-block" }}>
-					{getLabel(this.props.name)}
-				</div>
-			</div>
-		);
-	}
+        position: "relative"
+      },
+      flex: {
+        display: "flex",
+        alignItems: "center"
+      }
+    };
+
+    return (
+      <div className="menu-user" style={styles.flex}>
+        <div style={styles.image}>
+          {this.props.notifications > 0 ? (
+            <div className={"notification-bubble"}>
+              {this.props.notifications}
+            </div>
+          ) : null}
+          <Avatar src={this.props.picture} />
+        </div>
+        <div style={{ display: "inline-block", marginLeft: "9px" }}>
+          {getLabel(this.props.name)}
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = (state, ownProps) => {
-	return {
-		userId: state.login.profile.auth0Id,
-		notifications: state.notifications.data.length
-	};
+  return {
+    userId: state.login.profile.auth0Id,
+    notifications: state.notifications.data.length
+  };
 };
 
 export default connect(mapStateToProps)(UserMenuItem);
