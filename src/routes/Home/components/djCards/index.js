0f89a510feab2517bea93f98ca69bdbc653a2316
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-09-25 15:48:43 +0800

    cards

--- src/routes/Home/components/djCards/index.js
+++ src/routes/Home/components/djCards/index.js
@@ -1,32 +1,34 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./index.css";
 import { DJs } from "../../../../constants/constants";
+import shuffle from "lodash.shuffle";
 import DJCard from "../../../../components/common/DJCard";
-export default class DJCards extends Component {
-  render() {
-    return (
-      <div className="dj-cards">
-        <div>
-          {DJs.slice(0, 3).map((dj, idx) => (
-            <DJCard key={`dj-card-${idx}`} dj={dj} />
-          ))}
-        </div>
-        <div>
-          {DJs.slice(3, 6).map((dj, idx) => (
-            <DJCard key={`dj-card-${idx}`} dj={dj} />
-          ))}
-        </div>
-        <div>
-          {DJs.slice(6, 9).map((dj, idx) => (
-            <DJCard key={`dj-card-${idx}`} dj={dj} />
-          ))}
-        </div>
-        <div>
-          {DJs.slice(9, 11).map((dj, idx) => (
-            <DJCard key={`dj-card-${idx}`} dj={dj} />
-          ))}
-        </div>
+
+export default () => {
+  const [cards] = useState(shuffle(DJs));
+
+  return (
+    <div className="dj-cards">
+      <div>
+        {cards.slice(0, 3).map((dj, idx) => (
+          <DJCard key={`dj-card-${idx}`} dj={dj} />
+        ))}
       </div>
-    );
-  }
-}
+      <div>
+        {cards.slice(3, 6).map((dj, idx) => (
+          <DJCard key={`dj-card-${idx}`} dj={dj} />
+        ))}
+      </div>
+      <div>
+        {cards.slice(6, 9).map((dj, idx) => (
+          <DJCard key={`dj-card-${idx}`} dj={dj} />
+        ))}
+      </div>
+      <div>
+        {cards.slice(9, 11).map((dj, idx) => (
+          <DJCard key={`dj-card-${idx}`} dj={dj} />
+        ))}
+      </div>
+    </div>
+  );
+};
