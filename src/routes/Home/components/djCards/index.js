720b602f2ae36f97ec0368bfc746ace23fa16df7
Christopher Dengsø <christopherdengso@Christophers-MacBook-Pro.local>
2019-09-25 15:45:50 +0800

    dj cards

--- src/routes/Home/components/djCards/index.js
+++ src/routes/Home/components/djCards/index.js
@@ -1,73 +1,32 @@
 import React, { Component } from "react";
 import "./index.css";
 import { DJs } from "../../../../constants/constants";
-import shuffle from "lodash.shuffle";
 import DJCard from "../../../../components/common/DJCard";
 export default class DJCards extends Component {
-	animation = null;
-	cardsLoaded = false;
-	cards = [];
-
-	state = {
-		currentCards: [],
-		restCards: [],
-		cards: []
-	};
-
-	componentWillUnmount() {
-		this.animation && clearInterval(this.animation);
-	}
-
-	constructor(props) {
-		super(props);
-		this.cards = shuffle(
-			DJs.map((dj, idx) => (
-				<DJCard
-					{...props}
-					onLoad={() => this.cardsLoaded++}
-					key={`dj-card-${idx}`}
-					dj={dj}
-				/>
-			))
-		);
-	}
-
-	componentDidMount() {
-		const currentCards = this.cards.slice(0, 11);
-		const restCards = this.cards.slice(11, this.cards.length - 1);
-
-		this.setState({
-			currentCards,
-			restCards
-		});
-
-		// this.animation = setInterval(this.showNewCard, 4500);
-	}
-
-	showNewCard = () => {
-		const { currentCards, restCards } = this.state;
-		if (this.cardsLoaded >= currentCards.length) {
-			const idx = Math.floor(Math.random() * currentCards.length);
-			const newCard = restCards.pop();
-			const replacedCard = currentCards[idx];
-			currentCards[idx] = newCard;
-			this.setState({
-				restCards: [replacedCard, ...restCards],
-				currentCards
-			});
-		}
-	};
-
-	render() {
-		const { currentCards } = this.state;
-
-		return (
-			<div className="dj-cards">
-				<div>{currentCards.slice(0, 3)}</div>
-				<div>{currentCards.slice(3, 6)}</div>
-				<div>{currentCards.slice(6, 9)}</div>
-				<div>{currentCards.slice(9, 11)}</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="dj-cards">
+        <div>
+          {DJs.slice(0, 3).map((dj, idx) => (
+            <DJCard key={`dj-card-${idx}`} dj={dj} />
+          ))}
+        </div>
+        <div>
+          {DJs.slice(3, 6).map((dj, idx) => (
+            <DJCard key={`dj-card-${idx}`} dj={dj} />
+          ))}
+        </div>
+        <div>
+          {DJs.slice(6, 9).map((dj, idx) => (
+            <DJCard key={`dj-card-${idx}`} dj={dj} />
+          ))}
+        </div>
+        <div>
+          {DJs.slice(9, 11).map((dj, idx) => (
+            <DJCard key={`dj-card-${idx}`} dj={dj} />
+          ))}
+        </div>
+      </div>
+    );
+  }
 }
